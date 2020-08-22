301b2f8c370c29365592ad83c7b386c1b66b9050
Jason <chenjjer@hotmail.com>
2020-08-22 23:19:59 +0800

    Complile JNI file error modify

--- Charpter2/frameworks/com_android_server_FregService.cpp
+++ Charpter2/frameworks/com_android_server_FregService.cpp
@@ -12,7 +12,7 @@
 namespace android
 {
     static void freg_setVal(JNIEnv* env, jobject clazz, jint ptr, jint value) {
-        freg_device_t* device = (freg_device_t*)ptr;
+        freg_device_t* device = (freg_device_t*)(size_t)ptr;
         if (!device) {
             ALOGE("Device freg is not open.");
             return;
@@ -25,7 +25,7 @@ namespace android
     }
 
     static jint freg_getVal(JNIEnv* env, jobject clazz, jint ptr) {
-        freg_device_t* device = (freg_device_t*)ptr;
+        freg_device_t* device = (freg_device_t*)(size_t)ptr;
         if(!device) {
             ALOGE("Device freg is not open.");
             return 0;
@@ -38,41 +38,40 @@ namespace android
         return val;
     }
 
+
     static inline int freg_device_open(const hw_module_t* module, struct freg_device_t** device) {
-        return module->methods->open(module, FREG_HARDWARE_DEVICE_ID, (struct hw_device_t**)device);
-    }
+		return module->methods->open(module, FREG_HARDWARE_DEVICE_ID, (struct hw_device_t**)device);
+	}
 
-    static jint freg_init(JNIEnv* env, jclass clazz) {
-        freg_module_t* module;
-        freg_module_t* device;
+	static jint freg_init(JNIEnv* env, jclass clazz) {
+		freg_module_t* module;
+		freg_device_t* device;
 
-        ALOGI("Initializing HAL Stub freg ......");
+		ALOGI("Initializing HAL stub freg......");
 
-        if (hw_get_module(FREG_HARDWARE_MODULE_ID, (const struct hw_module_t**)&module) == 0) {
-            ALOGI("Device freg found.");
+		if(hw_get_module(FREG_HARDWARE_MODULE_ID, (const struct hw_module_t**)&module) == 0) {
+			ALOGI("Device freg found.");
+			if(freg_device_open(&(module->common), &device) == 0) {
+				ALOGI("Device freg is open.");
+				return (jint)(size_t)device;
+			}
 
-            if(freg_device_open(&(module->common), &device) == 0) {
-                ALOGI("Device freg is open.");
-                return (jint)device;
-            }
+			ALOGE("Failed to open device freg.");
+			return 0;
+		}
 
-            ALOGE("Failed to open device freg.");
-            return 0;
-        }
-        
-        ALOGE("Failed to get HAL stub freg.");
+		ALOGE("Failed to get HAL stub freg.");
 
-        return 0;
-    }
+		return 0;
+	}
 
-    static const JNINativeMethod method_table[] = {
-        { "init_native", "()I", (void*)freg_init},
-        { "setVal_native", "(II)V", (void*)freg_setVal},
-        { "getVal_native", "(I)I", (void*)freg_getVal},
-    };
+	static const JNINativeMethod method_table[] = {
+		{"init_native", "()I", (void*)freg_init},
+		{"setVal_native", "(II)V", (void*)freg_setVal},
+		{"getVal_native", "(I)I", (void*)freg_getVal},
+	};
 
-    int register_android_server_FregService(JNIEnv *env) {
-        return jniRegisterNativeMethod(env, "com/android/server/FregService",
-                method_table, NELEM(method_table));      
-    }
+	int register_android_server_FregService(JNIEnv *env) {
+ 		return jniRegisterNativeMethods(env, "com/android/server/FregService", method_table, NELEM(method_table));
+	}
 };
